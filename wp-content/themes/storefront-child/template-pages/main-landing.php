15bed37a05360b883deb952d2d4b8bf652a23f05
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-11 17:36:17 +0300

    Блок после хедера на главной

---
+++ wp-content/themes/storefront-child/template-pages/main-landing.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: main-landing
+Template Post Type: post, page, product
+*/
+?>
+
+<?php get_header(); ?>
+
+</div>
+</div>
+<div class="main-after-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-12">
+                <h1 class="main-after-header__title">Продажа запчастей</h1>
+                <p class="main-after-header__info">ДЛЯ ЧЕШСКОЙ ТЕХНИКИ ТZ-4К-14 (МТ8-132), MF-70, TERRA VARI</p>
+                <div class="card-traktor">
+                    <div class="card-traktor__body">
+                        <p class="card-traktor__title">Быстрый заказ</p>
+                        <p class="card-traktor__info">Если вы не хотите терять время на поиск необходимых запчастей,
+                            оставьте свой номер телефона, и менеджер свяжется с вами, чтобы быстро оформить заявку</p>
+                        <input class="input-traktor-primary" placeholder="+ 7 (_ _ _)-_ _-_ _" type="text">
+                        <button class="btn btn-traktor-primary">Оформить заявку</button>
+                        <p class="card-traktor__oferta">Нажимая кнопку “Оформить заявку” вы даете согласие на обработку
+                            своих персональных данных</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php get_footer(); ?>
