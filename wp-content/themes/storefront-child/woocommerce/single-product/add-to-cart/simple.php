f43441ae7face6c29a9bee4ddbd2473f60f53de9
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-12 17:12:42 +0300

    Страница продукта + изменение валюты

--- wp-content/themes/storefront-child/woocommerce/single-product/add-to-cart/simple.php
+++ wp-content/themes/storefront-child/woocommerce/single-product/add-to-cart/simple.php
@@ -15,42 +15,48 @@
  * @version 3.4.0
  */
 
-defined( 'ABSPATH' ) || exit;
+defined('ABSPATH') || exit;
 
 global $product;
 
-if ( ! $product->is_purchasable() ) {
-	return;
+if (!$product->is_purchasable()) {
+    return;
 }
 
-echo wc_get_stock_html( $product ); // WPCS: XSS ok.
+echo wc_get_stock_html($product); // WPCS: XSS ok.
 
-if ( $product->is_in_stock() ) : ?>
+if ($product->is_in_stock()) : ?>
 
-	<?php do_action( 'woocommerce_before_add_to_cart_form' ); ?>
+    <?php do_action('woocommerce_before_add_to_cart_form'); ?>
 
-	<form class="cart" action="<?php echo esc_url( apply_filters( 'woocommerce_add_to_cart_form_action', $product->get_permalink() ) ); ?>" method="post" enctype='multipart/form-data'>
-		<?php do_action( 'woocommerce_before_add_to_cart_button' ); ?>
+    <p class="product-info">Вес: <?= get_field('product_weight') ?></p>
+    <p class="product-info">Номер в каталоге: №<?= get_field('catalog_number') ?></p>
+    <form class="cart"
+          action="<?php echo esc_url(apply_filters('woocommerce_add_to_cart_form_action', $product->get_permalink())); ?>"
+          method="post" enctype='multipart/form-data'>
+        <p class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) ); ?>"><?php echo $product->get_price_html(); ?></p>
+        <?php do_action('woocommerce_before_add_to_cart_button'); ?>
 
-		<?php
-		do_action( 'woocommerce_before_add_to_cart_quantity' );
+        <?php
+        do_action('woocommerce_before_add_to_cart_quantity');
 
-		woocommerce_quantity_input(
-			array(
-				'min_value'   => apply_filters( 'woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product ),
-				'max_value'   => apply_filters( 'woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product ),
-				'input_value' => isset( $_POST['quantity'] ) ? wc_stock_amount( wp_unslash( $_POST['quantity'] ) ) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
-			)
-		);
+        woocommerce_quantity_input(
+            array(
+                'min_value' => apply_filters('woocommerce_quantity_input_min', $product->get_min_purchase_quantity(), $product),
+                'max_value' => apply_filters('woocommerce_quantity_input_max', $product->get_max_purchase_quantity(), $product),
+                'input_value' => isset($_POST['quantity']) ? wc_stock_amount(wp_unslash($_POST['quantity'])) : $product->get_min_purchase_quantity(), // WPCS: CSRF ok, input var ok.
+            )
+        );
 
-		do_action( 'woocommerce_after_add_to_cart_quantity' );
-		?>
+        do_action('woocommerce_after_add_to_cart_quantity');
+        ?>
 
-		<button type="submit" name="add-to-cart" value="<?php echo esc_attr( $product->get_id() ); ?>" class="single_add_to_cart_button button alt"><?php echo esc_html( $product->single_add_to_cart_text() ); ?></button>
+        <button type="submit" name="add-to-cart" value="<?php echo esc_attr($product->get_id()); ?>"
+                class="single_add_to_cart_button button alt"><?php echo esc_html($product->single_add_to_cart_text()); ?></button>
 
-		<?php do_action( 'woocommerce_after_add_to_cart_button' ); ?>
-	</form>
+        <?php do_action('woocommerce_after_add_to_cart_button'); ?>
+    </form>
 
-	<?php do_action( 'woocommerce_after_add_to_cart_form' ); ?>
+    <?php do_action('woocommerce_after_add_to_cart_form'); ?>
 
 <?php endif; ?>
--- wp-content/themes/storefront-child/woocommerce/single-product/short-description.php
+++ wp-content/themes/storefront-child/woocommerce/single-product/short-description.php
@@ -21,13 +21,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 global $post;
 
-$short_description = apply_filters( 'woocommerce_short_description', $post->post_excerpt );
-
-if ( ! $short_description ) {
-	return;
-}
-
 ?>
 <div class="woocommerce-product-details__short-description">
-	<?php echo $short_description; // WPCS: XSS ok. ?>
+    <?php the_content(); ?>
 </div>
