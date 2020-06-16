1a7d50ebafe29a78fca7a800b221059420894fba
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-16 16:47:18 +0300

    Инпут файлов

--- wp-content/themes/storefront-child/woocommerce/checkout/form-checkout.php
+++ wp-content/themes/storefront-child/woocommerce/checkout/form-checkout.php
@@ -28,40 +28,41 @@ if (!$checkout->is_registration_enabled() && $checkout->is_registration_required
 }
 
 ?>
-    <div class="col-lg-6 offset-lg-3 col-12 offset-0">
-        <form name="checkout" method="post" class="checkout woocommerce-checkout"
-              action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
+<div class="col-lg-6 offset-lg-3 col-12 offset-0">
+    <form name="checkout" method="post" class="checkout woocommerce-checkout"
+          action="<?php echo esc_url(wc_get_checkout_url()); ?>" enctype="multipart/form-data">
 
-            <?php if ($checkout->get_checkout_fields()) : ?>
+        <?php if ($checkout->get_checkout_fields()) : ?>
 
-                <?php do_action('woocommerce_checkout_before_customer_details'); ?>
+            <?php do_action('woocommerce_checkout_before_customer_details'); ?>
 
-                <div class="row" id="customer_details">
-                    <div class="col-12">
-                        <?php do_action('woocommerce_checkout_billing'); ?>
-                    </div>
-
-                    <div class="col-12">
-                        <?php do_action('woocommerce_checkout_shipping'); ?>
-                    </div>
+            <div class="row" id="customer_details">
+                <div class="col-12">
+                    <?php do_action('woocommerce_checkout_billing'); ?>
+                    <?php do_action('woocommerce_after_checkout_form', $checkout); ?>
                 </div>
 
-                <?php do_action('woocommerce_checkout_after_customer_details'); ?>
-
-            <?php endif; ?>
-
-            <?php do_action('woocommerce_checkout_before_order_review_heading'); ?>
-
-            <h3 id="order_review_heading"><?php esc_html_e('Your order', 'woocommerce'); ?></h3>
-
-            <?php do_action('woocommerce_checkout_before_order_review'); ?>
-
-            <div id="order_review" class="woocommerce-checkout-review-order">
-                <?php do_action('woocommerce_checkout_order_review'); ?>
+                <div class="col-12">
+                    <?php do_action('woocommerce_checkout_shipping'); ?>
+                </div>
             </div>
 
-            <?php do_action('woocommerce_checkout_after_order_review'); ?>
+            <?php do_action('woocommerce_checkout_after_customer_details'); ?>
 
-        </form>
-    </div>
+        <?php endif; ?>
+
+        <?php do_action('woocommerce_checkout_before_order_review_heading'); ?>
+
+        <h3 id="order_review_heading"><?php esc_html_e('Your order', 'woocommerce'); ?></h3>
+
+        <?php do_action('woocommerce_checkout_before_order_review'); ?>
+
+        <div id="order_review" class="woocommerce-checkout-review-order">
+            <?php do_action('woocommerce_checkout_order_review'); ?>
+        </div>
+
+        <?php do_action('woocommerce_checkout_after_order_review'); ?>
+
+    </form>
+</div>
 <?php do_action('woocommerce_after_checkout_form', $checkout); ?>
