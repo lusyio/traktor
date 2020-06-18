5784b98519541774b50046b99dbbaa3729efbca7
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-18 12:52:41 +0300

    Правки чекаута

--- wp-content/themes/storefront-child/functions.php
+++ wp-content/themes/storefront-child/functions.php
@@ -666,7 +666,7 @@ function custom_override_checkout_fields($fields)
     unset($fields['billing']['billing_address_2']);
     unset($fields['billing']['billing_city']);
 //    unset($fields['billing']['billing_postcode']);
-    unset($fields['billing']['billing_country']);
+//    unset($fields['billing']['billing_country']);
     unset($fields['billing']['billing_state']);
 //    unset($fields['billing']['billing_phone']);
     unset($fields['order']['order_comments']);
