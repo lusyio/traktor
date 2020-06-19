0e4e5b1e44e2c63a6f50efac2dda139bf42e82d4
Maxim Tretiak <48452049+mastekator@users.noreply.github.com>
2020-06-19 11:44:05 +0300

    страница 404

--- wp-content/themes/storefront-child/404.php
+++ wp-content/themes/storefront-child/404.php
@@ -15,63 +15,9 @@ get_header(); ?>
 
 				<div class="page-content">
 
-					<header class="page-header">
+					<header class="page-header pt-5 pb-5">
 						<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'storefront' ); ?></h1>
 					</header><!-- .page-header -->
-
-					<p><?php esc_html_e( 'Nothing was found at this location. Try searching, or check out the links below.', 'storefront' ); ?></p>
-
-					<?php
-					echo '<section aria-label="' . esc_html__( 'Search', 'storefront' ) . '">';
-
-					if ( storefront_is_woocommerce_activated() ) {
-						the_widget( 'WC_Widget_Product_Search' );
-					} else {
-						get_search_form();
-					}
-
-					echo '</section>';
-
-					if ( storefront_is_woocommerce_activated() ) {
-
-						echo '<div class="row">';
-
-							echo '<section class="col" aria-label="' . esc_html__( 'Promoted Products', 'storefront' ) . '">';
-
-								storefront_promoted_products();
-
-							echo '</section>';
-
-							echo '<nav class="col" aria-label="' . esc_html__( 'Product Categories', 'storefront' ) . '">';
-
-								echo '<h2>' . esc_html__( 'Product Categories', 'storefront' ) . '</h2>';
-
-								the_widget(
-									'WC_Widget_Product_Categories', array(
-										'count' => 1,
-									)
-								);
-
-							echo '</nav>';
-
-						echo '</div>';
-
-						echo '<section aria-label="' . esc_html__( 'Popular Products', 'storefront' ) . '">';
-
-							echo '<h2>' . esc_html__( 'Popular Products', 'storefront' ) . '</h2>';
-
-							$shortcode_content = storefront_do_shortcode(
-								'best_selling_products', array(
-									'per_page' => 4,
-									'columns'  => 4,
-								)
-							);
-
-							echo $shortcode_content; // WPCS: XSS ok.
-
-						echo '</section>';
-					}
-					?>
 
 				</div><!-- .page-content -->
 			</div><!-- .error-404 -->
